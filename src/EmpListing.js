84510195db8d2b8dd2afe7bb07a3c61f19dbcd47
Black-Mark <jeshuabay@gmail.com>
2023-04-24 14:43:08 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/EmpListing.js 	modified:   src/index.js

---
+++ src/EmpListing.js
@@ -0,0 +1,20 @@
+const EmpListing = () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h1>Employee Listing</h1>
+                </div>
+                <div className="card-body">
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                            <tr></tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default EmpListing;
